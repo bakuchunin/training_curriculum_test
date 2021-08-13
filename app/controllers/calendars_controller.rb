3b4a8846a2ee51cd3c111973af5a285af3087bec
bakuchunin <bakuchunin@gmail.com>
2021-08-13 13:39:03 +0900

    Issue4~6の解消３

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,8 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
-  end
+    params.require(:calendars)
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
@@ -41,7 +39,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x ).day, :plans => today_plans, :wday => wdays[wday_num]}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x ).day, :plans => today_plans, :wday => wday[wday_num]}
 
       @week_days.push(days)
     end
